64e34ba4916e1703cde34a55f3b7144de95d6788
Oto <oto_machavariani_ai@geolab.edu.ge>
2018-07-24 16:12:27 -0700

    firebase without fb authentication

--- Invitation/Invitation/AppDelegate.swift
+++ Invitation/Invitation/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,10 +17,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        
+        let storyboard = UIStoryboard(name: "Login", bundle: .main)
+        
+        
+        if let initialViewController = storyboard.instantiateInitialViewController() {
+            
+            window?.rootViewController = initialViewController
+            
+            window?.makeKeyAndVisible()
+        }
+        
+        
         return true
     }
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ Invitation/Pods/Target Support Files/Pods-Invitation/Pods-Invitation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Invitation : NSObject
+@end
+@implementation PodsDummy_Pods_Invitation
+@end
