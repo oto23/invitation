b10aeab4463db5baea7da2a1b01e350c7476610b
Talakhadze2001 <35135277+Talakhadze2001@users.noreply.github.com>
2018-07-26 11:16:32 -0700

    selection of friends

--- Invitation/Invitation/ViewController.swift
+++ Invitation/Invitation/ViewController.swift
@@ -9,26 +9,49 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    let listOfFriends = ["Sam", "Niko", "Oto"]
     
+    var reciever = [String]()
     
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    @IBAction func addButton(_ sender: Any) {
-        tableView.isHidden = false
+    @IBOutlet weak var friendsTableView: UITableView!
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return (listOfFriends.count)
     }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! UITableViewCell
+        cell.textLabel?.text = "Name and last name"
+        cell.textLabel?.textColor = #colorLiteral(red: 0.2745098039, green: 0.7803921569, blue: 0.02352941176, alpha: 1)
+        cell.backgroundColor = UIColor.darkGray
+        return (cell)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(listOfFriends[indexPath.row])
+        
+        // 1. preparing the data to send
+        reciever.append(listOfFriends[indexPath.row])
+        
+        // 2. change the ui of the cell
+        let cell = self.friendsTableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        friendsTableView.allowsMultipleSelection = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
