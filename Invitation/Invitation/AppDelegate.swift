f69d2455d2ecebf20210b882b8053a7bf1c26594
Oto <oto_machavariani_ai@geolab.edu.ge>
2018-07-26 01:44:31 -0700

    firebase login

--- Invitation/Invitation/AppDelegate.swift
+++ Invitation/Invitation/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
+import FBSDKCoreKit
 
 
 @UIApplicationMain
@@ -16,8 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         FirebaseApp.configure()
+        
+        
+        
         
         
         let storyboard = UIStoryboard(name: "Login", bundle: .main)
@@ -33,6 +40,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        let handled: Bool = FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        // Add any custom logic here.
+        return handled
+    }
+    
+
+    
+    
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ Invitation/Invitation/Controllers/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Invitation
+//
+//  Created by Macbook pro on 7/26/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    @IBOutlet weak var userEmailTextField: UITextField!
+    
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+}
